443b3815c575a595ada5c4826e3a2af9aaa7cc20
Oto Brglez <otobrglez@gmail.com>
2020-12-09 02:44:04 +0100

    Changing broker to broker.emqx.io

--- src/components/Config.ts
+++ src/components/Config.ts
@@ -1,6 +1,6 @@
 export class Config {
     static MAX_MEASUREMENTS_SIZE = 200;
     static MAX_EVENTS_SIZE = 20;
-    static MQTT_BROKER_URL = "ws://broker.hivemq.com:8000/mqtt";
+    static MQTT_BROKER_URL = "wss://broker.emqx.io:8084/mqtt";
     static DEFAULT_LIVE_VIEW_TAB = 'feed' // or list or events;
 }
